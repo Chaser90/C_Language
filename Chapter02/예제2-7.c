7809d6ad2e322d8de5da418dc0c649d556ab4d1d
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-11 20:59:27 +0900

    Add files via upload

---
+++ Chapter02/예제2-7.c
@@ -0,0 +1,10 @@
+//#include <stdio.h> //문자와 문자열 데이터의 출력
+//
+//int main(void)
+//{
+//	printf("%c\n", 'A'); //문자 상수 출력
+//	printf("%s\n", "A"); // 문자열 상수 출력
+//	printf("%c은 %s입니다.\n", '1', "first");
+//
+//	return 0;
+//}
