77e41bdd3835a1b7a9bf5ea87f257350f79ac5f9
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-11 21:01:44 +0900

    Add files via upload

---
+++ Chapter4/예제4-8.c
@@ -0,0 +1,18 @@
+////	04-2 그 외 유용한 연산자
+////	형 변환 연산자가 필요한 경우
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	int a = 20, b = 3;
+//	double res;
+//
+//	res = ((double)a) / ((double)b);					// (double)을 사용해 a와 b의 값6을 실수로 변환
+//	printf("a = %d, b = %d\n", a, b);
+//	printf("a / b 의 결과 : %.1lf\n", res);
+//
+//	a = (int)res;									// (int)를 사용해 res의 값에서 장스 브븐민 추림
+//	printf("(int) %.1lf의 결과 : %d\n", res, a);
+//
+//	return 0;
+//}
