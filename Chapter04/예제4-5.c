dd431c5ed27887e87ea11e4aa05026e2eb932b74
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-16 15:09:08 +0900

    Update 예제4-5.c

--- Chapter04/예제4-5.c
+++ Chapter04/예제4-5.c
@@ -7,7 +7,7 @@ int main(void)
 	int res;							// 결과 값을 저장할 변수
 										// 각각 a와 b, c값을 대입해보자. 
 	res = (a > b);						// 10 > 20은 거짓이므로 결과 값은 0
-  printf("a > b : %d\n", res);
+  	printf("a > b : %d\n", res);
 	res = (a >= b);						// 10 >= 20은 거짓이므로 결과 값은 0
 	printf("a >= b :%d\n", res);
 	res = (a <= b);						// 10 <= 20은 참이므로 결과 값은 1
