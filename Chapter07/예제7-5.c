6fcb4c184f479cd1e4e333f93233eace6b0675c9
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-16 16:13:42 +0900

    Update 예제7-5.c

--- Chapter07/예제7-5.c
+++ Chapter07/예제7-5.c
@@ -2,18 +2,18 @@
 
 #include<stdio.h>
 
-void fruit(void);							//	함수 선언
+void fruit(void);			//	함수 선언
 
 int main(void)
 {
-	fruit();							    	//	함수 호출
+	fruit();			//	함수 호출
 
 	return 0;
 }
 
-void fruit(void)							//	재귀호출 함수 정의
+void fruit(void)			//	재귀호출 함수 정의
 {
 	printf("apple\n");
-	fruit();							    	//	자신을 다시 호출
+	fruit();			//	자신을 다시 호출
 }
-                              //  ※ 멈추지 않고 계속 불러오기 실행되서 프로그램 과부하 발생
+                            		//  ※ 불러오는 횟수가 정해지지 않아 프로그램 과부하 발생
