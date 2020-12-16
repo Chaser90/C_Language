b51365b0a28a3de62cfae4e8e7c6d76cbd8f49fa
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-16 15:08:57 +0900

    Update 예제4-5.c

--- Chapter04/예제4-5.c
+++ Chapter04/예제4-5.c
@@ -1,23 +1,23 @@
-//// 관계 연산의 결과 값 확인
-//#include<stdio.h>
-//
-//int main(void)
-//{
-//	int a = 10, b = 20, c = 10;
-//	int res;							// 결과 값을 저장할 변수
-//										// 각각 a와 b, c값을 대입해보자. 
-//	res = (a > b);						// 10 > 20은 거짓이므로 결과 값은 0
-//	printf("a > b : %d\n", res);
-//	res = (a >= b);						// 10 >= 20은 거짓이므로 결과 값은 0
-//	printf("a >= b :%d\n", res);
-//	res = (a <= b);						// 10 <= 20은 참이므로 결과 값은 1
-//	printf("a <= b : %d\n", res);
-//	res = (a <= c);						// 10 <= 10은 참이므로 결과 값은 1
-//	printf("a <= c : %d\n", res);
-//	res = (a == b);						// 10 == 20은 거짓이므로 결과 값은 0
-//	printf("a == b : %d\n", res);
-//	res = (a != c);						// 10 != 10은 거짓이므로 결과 값은 0
-//	printf("a != c : %d\n", res);
-//
-//	return 0;
-//}
+// 관계 연산의 결과 값 확인
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 20, c = 10;
+	int res;							// 결과 값을 저장할 변수
+										// 각각 a와 b, c값을 대입해보자. 
+	res = (a > b);						// 10 > 20은 거짓이므로 결과 값은 0
+  printf("a > b : %d\n", res);
+	res = (a >= b);						// 10 >= 20은 거짓이므로 결과 값은 0
+	printf("a >= b :%d\n", res);
+	res = (a <= b);						// 10 <= 20은 참이므로 결과 값은 1
+	printf("a <= b : %d\n", res);
+	res = (a <= c);						// 10 <= 10은 참이므로 결과 값은 1
+	printf("a <= c : %d\n", res);
+	res = (a == b);						// 10 == 20은 거짓이므로 결과 값은 0
+	printf("a == b : %d\n", res);
+	res = (a != c);						// 10 != 10은 거짓이므로 결과 값은 0
+	printf("a != c : %d\n", res);
+
+	return 0;
+}
