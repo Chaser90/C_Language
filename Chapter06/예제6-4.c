f08d6a50af05f76adf87f1c678947432ed16c5e5
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-16 15:35:25 +0900

    Create 예제6-4.c

---
+++ Chapter06/예제6-4.c
@@ -0,0 +1,21 @@
+/*	06-2 반복문 활용
+	중첩 반복문을 사용한 별 출력
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 5; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
