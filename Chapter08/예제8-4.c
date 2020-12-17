335338a5085a0e537e27f857d68a52f5333fc548
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2020-12-17 16:37:24 +0900

    Create 예제8-4.c

---
+++ Chapter08/예제8-4.c
@@ -0,0 +1,17 @@
+/*	08-2 문자를 저장하는 배열
+	문자열을 저장하는 char형 배열
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	char str[80] = "applejam";					//	문자열 초기화
+	
+	printf("최초 문자열: %s\n", str);			//	초기화 문자열 출력
+	printf("문자열 입력 : ");
+	scanf("%s", str);							//	새로운 문자열 입력
+	printf("입력 후 문자열 : %s\n", str);		//	입력한 문자열 출력
+
+	return 0;
+}
