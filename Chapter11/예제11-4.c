3277e1b1b198e8c9c9ebc23aca394b20210698f5
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2021-01-21 16:28:39 +0900

    Create 예제11-4.c

---
+++ Chapter11/예제11-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// 버퍼를 사용하는 문자 입력
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 3; i++)			// 3번 반복
+	{
+		scanf("%c", &ch);			// 문자 입력
+		printf("%c", ch);			// 입력된 문자 출력
+	}
+
+	return 0;
+}
