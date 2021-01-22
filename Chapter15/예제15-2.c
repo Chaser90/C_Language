a96eedc9f59c5db1a2ccc7ae56c325a4cc1fd575
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2021-01-22 15:38:17 +0900

    Create 예제15-2.c

---
+++ Chapter15/예제15-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//	이중 포인터를 사용한 포인터 교환
+
+void swap_ptr(char** ppa, char** ppb);
+
+int main(void)
+{
+	char* pa = "success";
+	char* pb = "failure";
+
+	printf("pa -> %s, pb -> %s\n", pa, pb);		// 바꾸기 전에 문자열 출력
+	swap_ptr(&pa, &pb);							// 함수 호출
+	printf("pa -> %s, pb -> %s\n", pa, pb);		// 바꾸기 전에 문자열 출력
+	
+	return 0;
+}
+
+void swap_ptr(char** ppa, char** ppb)
+{
+	char* pt;
+	pt = *ppa;
+	*ppa = *ppb;
+	*ppb = pt;
+}
