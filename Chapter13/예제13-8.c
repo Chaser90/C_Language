495f4d5d1b2be0d679b02638a07e8701613fb0fe
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2021-01-22 11:28:32 +0900

    Create 예제13-8.c

---
+++ Chapter13/예제13-8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 주소를 반환하여 두 정수의 합 계산
+
+
+int *sum(int a, int b);			// int형 변수의 주소를 반환하는 함수 선언
+
+int main(void)
+{
+	int* resp;					// 반환값을 저장할 포인터 resp(result pointer)
+
+	resp = sum(10, 20);			// 반환된 주소는 resp에 저장
+	printf("두 정수의 합 : %d\n", *resp);		// resp가 가리키는 변수 값 출력
+
+	return 0;
+}
+
+int *sum(int a, int b)		// int형 변수의 주소를 반환하는 정수
+{
+	static int res;			// 정적 지역 변수
+
+	res = a + b;			// 두 정수의 합을 res(result)에 저장
+
+	return &res;			// 정적 지역 변수의 주소 반환
+}
