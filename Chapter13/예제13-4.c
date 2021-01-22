0163b55a0881cd2142db0c935766bb56010d846e
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2021-01-22 11:14:14 +0900

    Create 예제13-4.c

---
+++ Chapter13/예제13-4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//	auto 지역 변수와 static 지역 변수와 비교
+
+void auto_func(void);		// auto_func 함수 선언
+void static_func(void);		//static_func 함수 선언
+
+int main(void)
+{
+	int i;
+
+	printf("일반 지역 변수(auto)를 사용한 함수...\n");
+	
+	for (i = 0; i < 3; i++)
+	{
+		auto_func();
+	}
+
+	printf("정적 지역 변수(static)를 사용한 함수...\n");
+	for (i = 0; i < 3; i++)
+	{
+		static_func();
+	}
+
+	return 0;
+}
+
+void auto_func(void)
+{
+	auto int a = 0;			// 지역 변수 선언과 초기화
+
+	a++;					// a값 1 증가
+	printf("%d\n", a);		// a 출력
+}
+
+void static_func(void)
+{
+	static int a;			// 정적 지역 변수 선언
+
+	a++;					// a값 1 증가
+	printf("%d\n", a);		// a  출력
+}
