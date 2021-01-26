e5d89ff85cc96c9db23da710067e2fc94798172c
Chaser90 <54658653+Chaser90@users.noreply.github.com>
2021-01-26 20:28:55 +0900

    Create 17-12.c

---
+++ Chapter17/17-12.c
@@ -0,0 +1,27 @@
+// typedef를 사용한 자료형 재정의
+
+#include <stdio.h>
+
+struct student
+{
+	int num;
+	double grade;
+};
+
+typedef struct student Student;			// Student형으로 재정의
+void print_data(Student* ps);			// 매개 변수는 Student형의 포인터
+
+int main(void)
+{
+	Student s1 = { 315, 4.2 };			// Student형의 변수 선언과 초기화
+
+	print_data(&s1);					// Student형 변수의 주소 전달
+
+	return 0;
+}
+
+void print_data(Student* ps)
+{
+	printf("학번 : %d\n", ps->num);		// Student 포인터로 멤버 접근
+		printf("학점 : %.1lf\n", ps->grade);
+}
